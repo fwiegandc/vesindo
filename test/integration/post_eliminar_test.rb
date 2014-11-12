0ee0e20dcdaa7d47f25bb0796b947f39e6bb1a27
Francisco Wiegand <franciscowiegand@gmail.com>
2014-11-12 16:50:23 -0300

    Letra más grande en boxes

---
+++ test/integration/post_eliminar_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class PostEliminarTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "boton eliminar está disponible cuando corresponde" do
+  end
+
+  test "boton eliminar no está disponible para post de otros usuarios" do
+  end
+
+end
